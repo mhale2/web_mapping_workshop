08cb5e2f8cbf7b7fb0d1cf14a8691615055ed1d9
Madeline Hale <mhale2@gwu.edu>
2015-03-31 20:02:53 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'pk.eyJ1IjoibWpoYWxlMjAxNSIsImEiOiJZR2JLN2I4In0.NdJ9quPXAMLPBT4oqz9UCQ'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'mjhale2015.920fb783'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoibWpoYWxlMjAxNSIsImEiOiJZR2JLN2I4In0.NdJ9quPXAMLPBT4oqz9UCQ'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
